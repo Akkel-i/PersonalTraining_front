33713bdb8b3afad610e7a6299a5619e5fdb0c74a
Akkel-i <118986817+Akkel-i@users.noreply.github.com>
2024-05-05 15:35:04 +0300

    Update App.jsx

--- PersonalTrainer/src/App.jsx
+++ PersonalTrainer/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+//import './App.css'
 import { AppBar, Toolbar, Typography  } from "@mui/material"
 import Button from '@mui/material/Button';
 import { HashRouter } from "react-router-dom";
